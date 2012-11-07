8062794279d9809dd3142d25bf56f5ba913e3a31
okj <okj@knowit.no>
2012-11-07 09:35:00 +0100

    added parent directory autocomplete

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,7 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# autocomplete parent directory trailing slash (http://stackoverflow.com/a/716926)
+zstyle -e ':completion:*' special-dirs '[[ $PREFIX = (../)#(|.|..) ]] && reply=(..)'
+
