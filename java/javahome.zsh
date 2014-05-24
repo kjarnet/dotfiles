f878c558d6b7192f5e913f67d1c99c23ef4c6cd6
Oystein <gelefisk@gmail.com>
2014-05-24 22:03:56 +0200

    added java-8 path.

--- java/javahome.zsh
+++ java/javahome.zsh
@@ -1,5 +1,6 @@
 # linux
-export JAVA_HOME="/usr/lib/jvm/java-7-oracle/"
+# export JAVA_HOME="/usr/lib/jvm/java-7-oracle/"
+export JAVA_HOME="/usr/lib/jvm/java-8-oracle/"
 
 # osx:
 # export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_45.jdk/Contents/Home/"
