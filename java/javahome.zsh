cb183a51938c4877564eec1737938c113de2c386
Oystein <gelefisk@gmail.com>
2014-08-02 13:59:33 +0200

    Added host aliases to ssh config example.

--- java/javahome.zsh
+++ java/javahome.zsh
@@ -3,5 +3,5 @@
 export JAVA_HOME="/usr/lib/jvm/java-8-oracle/"
 
 # osx:
-# export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_45.jdk/Contents/Home/"
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_45.jdk/Contents/Home/"
 
