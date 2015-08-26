7d4b81539fc0059a8fbda48c56eb5b7c11bf9674
Øystein Kjærnet <kjarnet@gmail.com>
2015-08-26 07:54:03 +0200

    Added java_home for java 8 on osx.

--- java/javahome.zsh
+++ java/javahome.zsh
@@ -1,7 +1,8 @@
 # linux
 # export JAVA_HOME="/usr/lib/jvm/java-7-oracle/"
-export JAVA_HOME="/usr/lib/jvm/java-8-oracle/"
+# export JAVA_HOME="/usr/lib/jvm/java-8-oracle/"
 
 # osx:
 # export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_45.jdk/Contents/Home/"
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_51.jdk/Contents/Home/"
 
