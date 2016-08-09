7eddfe3dbe250f3170ade9481bd2328f32c6a75c
Øystein Kjærnet <kjarnet@gmail.com>
2016-08-09 10:27:55 +0200

    Don't go to parent dir in bootstrap.sh

--- java/javahome.zsh
+++ java/javahome.zsh
@@ -1,8 +1,9 @@
 # linux
 # export JAVA_HOME="/usr/lib/jvm/java-7-oracle/"
 # export JAVA_HOME="/usr/lib/jvm/java-8-oracle/"
+export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/"
 
 # osx:
 # export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_45.jdk/Contents/Home/"
-export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_66.jdk/Contents/Home/"
+# export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_66.jdk/Contents/Home/"
 
