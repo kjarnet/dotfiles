a48e7aaaec02ec9a1c3591a2213bf270447ec8eb
Øystein Kjærnet <oystein.kjarnet@tomra.com>
2021-09-10 10:23:36 +0200

    added short-hand functions for aws ssm commands

--- zsh/aws.zsh
+++ zsh/aws.zsh
@@ -1,2 +1,5 @@
 alias aws-ecr-login='$(aws ecr get-login --no-include-email)'
+ssm-get() { aws ssm get-parameter --with-decryption  --output text --query Parameter.Value --profile $2 --name "$1"; }
+ssm-put() { aws ssm put-parameter --type SecureString --profile $3 --name "$1" --value "$2"; }
+ssm-update() { aws ssm put-parameter --overwrite --type SecureString --profile $3 --name "$1" --value "$2"; }
 
--- zsh/java.zsh
+++ zsh/java.zsh
@@ -1,3 +1,5 @@
+alias java15='export JAVA_HOME=$(/usr/libexec/java_home -v 15)'
+alias java14='export JAVA_HOME=$(/usr/libexec/java_home -v 14)'
 alias java13='export JAVA_HOME=$(/usr/libexec/java_home -v 13)'
 alias java12='export JAVA_HOME=$(/usr/libexec/java_home -v 12)'
 alias java11='export JAVA_HOME=$(/usr/libexec/java_home -v 11)'
