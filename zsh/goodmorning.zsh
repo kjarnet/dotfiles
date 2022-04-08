35b8e4b63e977c96d559b7a56aec26c6ad9c5f16
Øystein Kjærnet <kjarnet@users.noreply.github.com>
2022-04-08 22:44:23 +0200

    added aws cli shortcuts and more

---
+++ zsh/goodmorning.zsh
@@ -0,0 +1,7 @@
+# Stuff to run in the morning
+goodmorning () {
+    echo "Good morning!"
+    echo "Running brew update && brew upgrade"
+    brew update && brew upgrade;
+}
+
--- zsh/java.zsh
+++ zsh/java.zsh
@@ -1,3 +1,4 @@
+alias java17='export JAVA_HOME=$(/usr/libexec/java_home -v 17)'
 alias java15='export JAVA_HOME=$(/usr/libexec/java_home -v 15)'
 alias java14='export JAVA_HOME=$(/usr/libexec/java_home -v 14)'
 alias java13='export JAVA_HOME=$(/usr/libexec/java_home -v 13)'
