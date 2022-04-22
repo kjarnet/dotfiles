d55be6e0e682b3b966b9a4c8e56fbd65a43ccc07
Oystein Kjarnet <kjarnet@users.noreply.github.com>
2022-04-22 10:56:11 +0200

    iterm: indicate zsh vim mode

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -1,4 +1,27 @@
 bindkey -v
 setopt globdots
+
+# Indicate vim mode
+function zle-keymap-select zle-line-init
+{
+    # change cursor shape in iTerm2
+    case $KEYMAP in
+        vicmd)      print -n -- "\E]50;CursorShape=0\C-G";;  # block cursor
+        viins|main) print -n -- "\E]50;CursorShape=2\C-G";;  # underscore cursor
+    esac
+
+    zle reset-prompt
+    zle -R
+}
+
+function zle-line-finish
+{
+    print -n -- "\E]50;CursorShape=0\C-G"  # block cursor
+}
+
+zle -N zle-line-init
+zle -N zle-line-finish
+zle -N zle-keymap-select
+
 echo "applied settings"
 
